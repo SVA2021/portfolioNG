97b9edf8a56b471201370f6ca0baeca5230499e8
SVA2021 <sofronov.vit@gmail.com>
2023-02-10 16:45:55 +0400

    add svg sprites

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,19 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 import {LayoutModule} from "./layout/layout.module";
 import {AppRoutingModule} from "./app-routing.module";
+import {HomePageComponent} from "./pages/home-page/home-page.component";
+import {SharedModule} from "./shared/shared.module";
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomePageComponent,
   ],
   imports: [
     BrowserModule,
     LayoutModule,
     AppRoutingModule,
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
