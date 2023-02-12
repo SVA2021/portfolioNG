2cf7d8f6c0f04494eeaa6887eb19e33d8c91fb69
SVA2021 <sofronov.vit@gmail.com>
2023-02-12 16:17:25 +0400

    add about page

--- src/app/pages/about-page/about-page.component.ts
+++ src/app/pages/about-page/about-page.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {AboutService} from "../../services/about.service";
+import {ghStatsItemI} from "../../interfaces/about";
 
 @Component({
   selector: 'app-about-page',
@@ -14,6 +15,13 @@ export class AboutPageComponent implements OnInit {
   skillTitle: string
   mySkills: string[]
   iconSrc: string
+  ghTitle: string
+  ghStats: ghStatsItemI
+  ghMostUsed: ghStatsItemI
+  certificateTitle: string
+  certificateTip: string
+  certificateImgSrc: string
+  certificateImgAlt: string
 
   constructor(
     public about: AboutService
@@ -23,7 +31,14 @@ export class AboutPageComponent implements OnInit {
     this.bioFull = '';
     this.skillTitle = '';
     this.mySkills = [];
-    this.iconSrc = 'src/assets/skills.svg';
+    this.iconSrc = '';
+    this.ghTitle = '';
+    this.ghStats = {} as ghStatsItemI;
+    this.ghMostUsed = {} as ghStatsItemI;
+    this.certificateTitle = '';
+    this.certificateTip = '';
+    this.certificateImgSrc = '';
+    this.certificateImgAlt = '';
   }
 
   ngOnInit(): void {
@@ -32,7 +47,14 @@ export class AboutPageComponent implements OnInit {
     this.bioFull = this.about.bioFull;
     this.skillTitle = this.about.skillTitle;
     this.mySkills = this.about.mySkills;
-    // this.iconSrc = this.about.svgIconSrc;
+    this.iconSrc = this.about.svgIconSrc;
+    this.ghTitle = this.about.ghTitle;
+    this.ghStats = this.about.ghStats;
+    this.ghMostUsed = this.about.ghMostUsed;
+    this.certificateTitle = this.about.certificateTitle;
+    this.certificateTip = this.about.certificateTip;
+    this.certificateImgSrc = this.about.certificateImgSrc;
+    this.certificateImgAlt = this.about.certificateImgAlt;
   }
 
 }
