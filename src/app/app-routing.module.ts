16c2ad2a2f856908245df48dfc740f2442e6f2fe
SVA2021 <sofronov.vit@gmail.com>
2023-02-27 16:09:21 +0400

    add config to scroll top at routes change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -17,7 +17,8 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes),
+    RouterModule.forRoot(routes,
+      {scrollPositionRestoration: "top"}),
   ],
   exports: [RouterModule]
 })
