f45ec366e87bcb247547e3d5f9195e1ff54df313
SVA2021 <sofronov.vit@gmail.com>
2023-02-28 14:05:24 +0400

    add routes to admin and login pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,9 +5,13 @@ import {AboutPageComponent} from "./pages/about-page/about-page.component";
 import {ContactPageComponent} from "./pages/contact-page/contact-page.component";
 import {CertificatesPageComponent} from "./pages/certificates-page/certificates-page.component";
 import {ProjectsPageComponent} from "./pages/projects-page/projects-page.component";
+import {AdminPageComponent} from "./pages/admin-page/admin-page.component";
+import {LoginPageComponent} from "./pages/login-page/login-page.component";
 
 const routes: Routes = [
   {path: '', component: HomePageComponent},
+  {path: 'admin', component: AdminPageComponent},
+  {path: 'login', component: LoginPageComponent},
   {path: 'about', component: AboutPageComponent},
   {path: 'contacts', component: ContactPageComponent},
   {path: 'projects', component: ProjectsPageComponent},
