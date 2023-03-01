413a8b5fe9461e42ea42114d1f131825af84c458
SVA2021 <sofronov.vit@gmail.com>
2023-03-01 21:28:46 +0400

    upd form initial values and submit

--- src/app/shared/components/future-project-form/future-project-form.component.ts
+++ src/app/shared/components/future-project-form/future-project-form.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {futureProjectT} from "../../../interfaces/projects";
 
@@ -10,16 +10,18 @@ import {futureProjectT} from "../../../interfaces/projects";
 export class FutureProjectFormComponent implements OnInit, OnDestroy {
 
   future: FormGroup
+  @Input() initialProject: futureProjectT | undefined | null
+  @Input() submitHandler!: (project: futureProjectT) => void
 
   constructor() {
     this.future = new FormGroup({
-        name: new FormControl('', [Validators.required, Validators.minLength(3)]),
-        descriptionEn: new FormControl('', [Validators.required, Validators.minLength(3),]),
-        descriptionRu: new FormControl('', [Validators.required, Validators.minLength(3),]),
-        whyEn: new FormControl('', [Validators.required, Validators.minLength(3),]),
-        whyRu: new FormControl('', [Validators.required, Validators.minLength(3),]),
-        technologies: new FormControl('', [Validators.required, Validators.minLength(3),]),
-        newSkills: new FormControl('', [Validators.required, Validators.minLength(3),]),
+        name: new FormControl(this.initialProject?.name ?? '', [Validators.required, Validators.minLength(3)]),
+        descriptionEn: new FormControl(this.initialProject?.description.en ?? '', [Validators.required, Validators.minLength(3),]),
+        descriptionRu: new FormControl(this.initialProject?.description.ru ?? '', [Validators.required, Validators.minLength(3),]),
+        whyEn: new FormControl(this.initialProject?.why.en ?? '', [Validators.required, Validators.minLength(3),]),
+        whyRu: new FormControl(this.initialProject?.why.ru ?? '', [Validators.required, Validators.minLength(3),]),
+        technologies: new FormControl(this.initialProject?.technologies ?? '', [Validators.required, Validators.minLength(3),]),
+        newSkills: new FormControl(this.initialProject?.newSkills ?? '', [Validators.required, Validators.minLength(3),]),
       }
     );
   }
@@ -44,11 +46,11 @@ export class FutureProjectFormComponent implements OnInit, OnDestroy {
       newSkills: this.future.value.newSkills,
     }
 
-    console.log(newFutureProject)
+    this.submitHandler(newFutureProject);
     // this.future.reset();
   }
 
   ngOnDestroy(): void {
-    // this.future.reset();
+    this.future.reset();
   }
 }
