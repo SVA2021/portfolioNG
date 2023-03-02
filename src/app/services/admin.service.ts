86d914f63ce8acaec2ceb922d562952ed0903bd2
SVA2021 <sofronov.vit@gmail.com>
2023-03-02 17:01:13 +0400

    add webskill array

--- src/app/services/admin.service.ts
+++ src/app/services/admin.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import {WEB_SKILLS_FULL, webSkill} from "../interfaces/admin";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminService {
 
-  constructor() { }
+  webSkillsFull : webSkill[]
+
+  constructor() {
+    this.webSkillsFull = [...WEB_SKILLS_FULL];
+  }
 }
