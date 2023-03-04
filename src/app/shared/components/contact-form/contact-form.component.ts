fe5d9e0997b4eac49a99a379f83dfd4dce4481a7
SVA2021 <sofronov.vit@gmail.com>
2023-03-04 12:07:02 +0400

    add auth data to contact form

--- src/app/shared/components/contact-form/contact-form.component.ts
+++ src/app/shared/components/contact-form/contact-form.component.ts
@@ -1,44 +1,62 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {
+  AfterContentChecked,
+  Component,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {formFields, formWarnings} from "../interfaces";
 import {LangService} from "../../../layout/services/lang.service";
 import {ContactFormService} from "../../services";
 import {Subscription} from "rxjs";
 import {SmartdevService} from "../../../services/smartdev.service";
+import {AuthService} from "../../../services/auth.service";
 
 @Component({
   selector: 'app-contact-form',
   templateUrl: './contact-form.component.html',
   styleUrls: ['./contact-form.component.scss']
 })
-export class ContactFormComponent implements OnInit, OnDestroy {
+export class ContactFormComponent implements OnInit, OnDestroy, AfterContentChecked {
 
   contacts: FormGroup
   fields: formFields
   warnings: formWarnings
   myLang$: Subscription
+  user$: Subscription
+  userName: string
+  userEmail: string
 
   constructor(
     public lang: LangService,
     public contactFormService: ContactFormService,
-    public smartdev: SmartdevService
+    public smartDev: SmartdevService,
+    public auth: AuthService,
   ) {
-    this.contacts = new FormGroup({
-        name: new FormControl('', [Validators.required, Validators.minLength(3)]),
-        email: new FormControl('', [Validators.required, Validators.email]),
-        message: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(240)]),
-      }
-    );
+    this.contacts = new FormGroup({});
     this.fields = this.contactFormService.contactForm[this.lang.lastLang].fields;
     this.warnings = this.contactFormService.contactForm[this.lang.lastLang].warnings;
     this.myLang$ = Subscription.EMPTY;
+    this.user$ = Subscription.EMPTY;
+    this.userName = '';
+    this.userEmail = '';
   }
 
   ngOnInit(): void {
     this.myLang$ = this.lang.activeLang$.subscribe(lang => {
       this.fields = this.contactFormService.contactForm[lang].fields;
       this.warnings = this.contactFormService.contactForm[lang].warnings;
-    })
+    });
+    this.user$ = this.auth.user.subscribe(user => {
+      this.userName = user.displayName ?? '';
+      this.userEmail = user.email ?? '';
+    });
+    this.contacts = new FormGroup({
+        name: new FormControl(this.userName ?? '', [Validators.required, Validators.minLength(3)]),
+        email: new FormControl(this.userEmail ?? '', [Validators.required, Validators.email]),
+        message: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(240)]),
+      }
+    );
   }
 
   submit() {
@@ -49,12 +67,21 @@ export class ContactFormComponent implements OnInit, OnDestroy {
     smartData.append('email', this.contacts.value.email);
     smartData.append('message', this.contacts.value.message);
 
-    this.smartdev.sendForm(smartData);
+    this.smartDev.sendForm(smartData);
     this.contacts.reset();
   }
 
   ngOnDestroy(): void {
     this.myLang$.unsubscribe();
+    this.user$.unsubscribe();
     this.contacts.reset();
   }
+
+  ngAfterContentChecked(): void {
+    this.contacts.patchValue({
+      name: this.userName ?? '',
+      email: this.userEmail ?? '',
+    })
+  }
+
 }
