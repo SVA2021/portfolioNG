50f39d1033faf61a0aff29f940979297276d76c3
SVA2021 <sofronov.vit@gmail.com>
2023-03-04 12:29:45 +0400

    move projects data to firebase

--- src/app/pages/projects-page/projects-page.component.ts
+++ src/app/pages/projects-page/projects-page.component.ts
@@ -3,6 +3,7 @@ import {ProjectsService} from "../../services/projects.service";
 import {LangService} from "../../layout/services/lang.service";
 import {actualProjectT, futureProjectT, pageTitlesT,} from "../../interfaces/projects";
 import {Subscription} from "rxjs";
+import {FirebaseService} from "../../services/firebase.service";
 
 @Component({
   selector: 'app-projects-page',
@@ -15,26 +16,38 @@ export class ProjectsPageComponent implements OnInit, OnDestroy {
   pageTitles: pageTitlesT
   futureProjects: futureProjectT[]
   actualProjects: actualProjectT[]
+  actualFbProjects$: Subscription
+  futureFbProjects$: Subscription
 
   constructor(
     public projects: ProjectsService,
     public lang: LangService,
+    public fb: FirebaseService,
   ) {
     this.myLang$ = Subscription.EMPTY;
     this.pageTitles = this.projects.pageTitles[this.lang.lastLang];
-    this.futureProjects = this.projects.futureProjects;
-    this.actualProjects = this.projects.actualProjects;
+    this.futureProjects = [];
+    this.actualProjects = [];
+    this.actualFbProjects$ = Subscription.EMPTY;
+    this.futureFbProjects$ = Subscription.EMPTY;
   }
 
   ngOnInit(): void {
-    this.myLang$ = this.lang.activeLang$.subscribe(lang => {
-        this.pageTitles = this.projects.pageTitles[lang];
-      }
-    )
+    this.myLang$ = this.lang.activeLang$.subscribe(lang =>
+      this.pageTitles = this.projects.pageTitles[lang]
+    );
+    this.actualFbProjects$ = this.fb.actualProjectsFb$.subscribe(store =>
+      this.actualProjects = store
+    );
+    this.futureFbProjects$ = this.fb.futureProjectsFb$.subscribe(store =>
+      this.futureProjects = store
+    );
   }
 
   ngOnDestroy(): void {
     this.myLang$.unsubscribe();
+    this.actualFbProjects$.unsubscribe();
+    this.futureFbProjects$.unsubscribe();
   }
 
 }
